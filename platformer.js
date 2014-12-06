79be57ee10d294611729708e85233e3d9e2acdf7
CM137 <ChrisMason137@gmail.com>
2014-12-06 16:24:26 -0500

    Animations
    tweaks

--- platformer.js
+++ platformer.js
@@ -583,14 +583,14 @@ Q.load("spritesheet2.json, spritesheet2.png, level1.json, level2.json, level3.js
   // Or from a .json asset that defines sprite locations
   Q.compileSheets("spritesheet2.png", "spritesheet2.json");
   Q.animations('player', {
-      walk_right: { frames: [0,1,2,3,4,5,6,7,8], rate: 1/6, flip: false, loop: true },
-      walk_left: { frames:  [1,2,3,4,5,6,7], rate: 1/6, flip: "x", loop: true },
+      walk_right: { frames: [0,1,2,3,4,5,6,7], rate: 1/6, flip: false, loop: true },
+      walk_left: { frames:  [0,1,2,3,4,5,6,7], rate: 1/6, flip: "x", loop: true },
       jump_right: { frames: [16], rate: 1/1, flip: false },
       jump_left: { frames:  [17], rate: 1/1, flip: false },
       fall_right: { frames:  [18], rate: 1/1, flip: false },
       fall_left: { frames:  [19], rate: 1/1, flip: false },
-      stand_right: { frames:[2], rate: 1/1, flip: false },
-      stand_left: { frames: [2], rate: 1/1, flip: "x" },
+      stand_right: { frames:[8], rate: 1/1, flip: false },
+      stand_left: { frames: [8], rate: 1/1, flip: "x" },
   });
   
   Q.animations('coins', {
